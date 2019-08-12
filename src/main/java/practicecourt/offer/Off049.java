cd8312d09edfcf5fc256e2bc95bbbc8567f83b62
fengh <fh75104423@163.com>
2019-08-13 00:35:50 +0800

    约瑟夫问题还未解决。。

--- src/main/java/practicecourt/offer/Off049.java
+++ src/main/java/practicecourt/offer/Off049.java
@@ -1,36 +1,58 @@
 package practicecourt.offer;
 
 /**
- * 每年六一儿童节,牛客都会准备一些小礼物去看望孤儿院的小朋友,今年亦是如此。
- * HF作为牛客的资深元老,自然也准备了一些小游戏。其中,有个游戏是这样的:首先,让小朋友们围成一个大圈。
- * 然后,他随机指定一个数m,让编号为0的小朋友开始报数。每次喊到m-1的那个小朋友要出列唱首歌,
- * 然后可以在礼品箱中任意的挑选礼物,并且不再回到圈中,从他的下一个小朋友开始,继续0...m-1报数....这样下去....
- * 直到剩下最后一个小朋友,可以不用表演,并且拿到牛客名贵的“名侦探柯南”典藏版(名额有限哦!!^_^)。
- * 请你试着想下,哪个小朋友会得到这份礼品呢？(注：小朋友的编号是从0到n-1)
+ * 每年六一儿童节,牛客都会准备一些小礼物去看望孤儿院的小朋友,今年亦是如此。 HF作为牛客的资深元老,自然也准备了一些小游戏。其中,有个游戏是这样的:首先,让小朋友们围成一个大圈。
+ * 然后,他随机指定一个数m,让编号为0的小朋友开始报数。每次喊到m-1的那个小朋友要出列唱首歌, 然后可以在礼品箱中任意的挑选礼物,并且不再回到圈中,从他的下一个小朋友开始,继续0...m-1报数....这样下去....
+ * 直到剩下最后一个小朋友,可以不用表演,并且拿到牛客名贵的“名侦探柯南”典藏版(名额有限哦!!^_^)。 请你试着想下,哪个小朋友会得到这份礼品呢？(注：小朋友的编号是从0到n-1)
  * <p>
  * 约瑟夫环
  */
 public class Off049 {
-    public int lastRemainingSolution(int n, int m) {
-        Node pre = null;
 
+    public int lastRemainingSolution(int n, int m) {
+        if (n < 1 || m < 1) {
+            return -1;
+        }
+
+        Node tmp = null, pre = null;
+
+        // 构建环
         for (int i = 0; i < n; i++) {
             Node node = new Node(i);
-            if (pre != null) {
-                pre = node.pre;
+            if (i == 0) {
+                pre = node;
+                tmp = node;
+                continue;
             }
-            pre = node;
-
+            tmp.next = node;
+            tmp = node;
         }
+        if (pre != tmp) {
+            tmp = tmp.next = pre;
+        }
+
+        while (n > 1) {
+            for (int i = 0; i < m; i++) {
+                pre = tmp;
+                tmp = tmp.next;
+            }
+            tmp = pre.next = tmp.next;
+            n--;
+        }
+        return tmp.val;
     }
 }
 
 class Node {
+
     public int val;
-    public Node pre;
     public Node next;
 
     public Node(int val) {
         this.val = val;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
     }
 }
---
+++ src/test/java/practicecourt/offer/Off049Test.java
@@ -0,0 +1,14 @@
+package practicecourt.offer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Off049Test {
+
+    @Test
+    public void lastRemainingSolution() {
+        int i = new Off049().lastRemainingSolution(5, 3); // 3?
+        System.out.println(i);
+    }
+}
